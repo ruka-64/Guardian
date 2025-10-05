7a97e2bb786e20df91c42d7cc74542414a343922
ruka-64 <202770393+ruka-64@users.noreply.github.com>
2025-10-05 19:14:20 +0900

    fu

--- src/bot/utils/autoFight.ts
+++ src/bot/utils/autoFight.ts
@@ -7,18 +7,8 @@ export const autoAttackEntity = (activate: boolean) => {
   if (activate) {
     attackInterval = setInterval(() => {
       const entity = bot.nearestEntity((e) => {
-        console.log(
-          '(e,xz,y)',
-          e.name,
-          e.position.xzDistanceTo(bot.entity.position),
-          bot.entity.position.y - e.position.y
-        );
         return (
-          e.type != 'player' &&
-          e.type != 'orb' &&
-          e.type != 'object' &&
-          e.type != 'projectile' &&
-          e.type != 'hostile' &&
+          e.type === 'hostile' &&
           e.position.xzDistanceTo(bot.entity.position) < 5 &&
           e.position.y - bot.entity.position.y < 2 &&
           e.name !== 'dog' &&
