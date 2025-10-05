d98fc648840ec3750e6e75174551078d469a9e03
ruka-64 <202770393+ruka-64@users.noreply.github.com>
2025-10-05 23:58:23 +0900

    rotation fix again

--- src/bot/utils/inv.ts
+++ src/bot/utils/inv.ts
@@ -9,7 +9,7 @@ export const InvCleaner = async () => {
   const bone_id = bot.registry.itemsByName.bone!.id;
   const arrow_id = bot.registry.itemsByName.arrow!.id;
   const e = bot.entity;
-  await bot.look(e.yaw + 90, e.pitch);
+  await bot.look(e.yaw - 90, e.pitch);
   const rotten_flesh = bot.inventory.findInventoryItem(
     rotten_flesh_id,
     null,
@@ -20,5 +20,5 @@ export const InvCleaner = async () => {
   if (rotten_flesh) await bot.toss(rotten_flesh.type, null, rotten_flesh.count);
   if (bone) await bot.toss(bone.type, null, bone.count);
   if (arrow) await bot.toss(arrow.type, null, arrow.count);
-  await bot.look(e.yaw - 90, e.pitch);
+  await bot.look(e.yaw + 90, e.pitch);
 };
