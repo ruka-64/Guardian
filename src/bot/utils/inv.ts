8cf3fbf97ba7943f4d77709a55fe1e8bcdbeaf8a
ruka-64 <202770393+ruka-64@users.noreply.github.com>
2025-10-06 21:25:20 +0900

    fix invcleaner

--- src/bot/utils/inv.ts
+++ src/bot/utils/inv.ts
@@ -5,21 +5,15 @@ export const isInvFull = () => {
 };
 
 export const InvCleaner = async () => {
-  const rotten_flesh_id = bot.registry.itemsByName.rotten_flesh!.id;
-  const bone_id = bot.registry.itemsByName.bone!.id;
-  const arrow_id = bot.registry.itemsByName.arrow!.id;
   const e = bot.entity;
   await bot.look(e.yaw - 180, e.pitch);
-  const rotten_flesh = bot.inventory.findInventoryItem(
-    rotten_flesh_id,
-    null,
-    false
-  );
-  const bone = bot.inventory.findInventoryItem(bone_id, null, false);
-  const arrow = bot.inventory.findInventoryItem(arrow_id, null, false);
-  if (rotten_flesh) await bot.toss(rotten_flesh.type, null, rotten_flesh.count);
-  if (bone) await bot.toss(bone.type, null, bone.count);
-  if (arrow) await bot.toss(arrow.type, null, arrow.count);
+  for (const slot of bot.inventory.slots) {
+    if (!slot) continue;
+    if (slot.name === 'bone') await bot.toss(slot.type, null, slot.count);
+    if (slot.name === 'arrow') await bot.toss(slot.type, null, slot.count);
+    if (slot.name === 'rotten_flesh')
+      await bot.toss(slot.type, null, slot.count);
+  }
   await bot.look(e.yaw + 180, e.pitch);
 };
 
