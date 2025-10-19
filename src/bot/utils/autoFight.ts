32108b01655147e8ea88af4d3275706ecf488bae
ruka-64 <202770393+ruka-64@users.noreply.github.com>
2025-10-19 18:10:50 +0900

    change delay

--- src/bot/utils/autoFight.ts
+++ src/bot/utils/autoFight.ts
@@ -37,7 +37,7 @@ export const autoAttackEntity = async (activate: boolean, move = false) => {
         await bot.waitForTicks(1);
         bot.attack(entity);
       }
-    }, 10000);
+    }, 8000);
   } else {
     clearInterval(attackInterval);
     logger.info('Calling /home botpos');
